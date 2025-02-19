f72a084a4ca5187cba1043f932559672781c8d36
shlmita <shelomita.fitriyani@gmail.com>
2025-02-19 21:55:40 +0700

    perbaikan asset

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -3,8 +3,8 @@ import { motion, AnimatePresence } from "framer-motion";
 
 
 const slides = [
-    { image: "/proyek-1.jpg", title: "Judul Slide 1", description: "Deskripsi singkat slide pertama." },
-    { image: "/proyek-2.jpeg", title: "Judul Slide 2", description: "Deskripsi singkat slide kedua." },
+    { image: "https://pupr-eta.vercel.app/proyek-1.jpg", title: "Judul Slide 1", description: "Deskripsi singkat slide pertama." },
+    { image: "https://pupr-eta.vercel.app/proyek-2.jpeg", title: "Judul Slide 2", description: "Deskripsi singkat slide kedua." },
 ];
 
 const Carousel = () => {
