e0ae9950de1280d6db8bda8ec741f93dcacdc942
shlmita <shelomita.fitriyani@gmail.com>
2025-05-13 16:39:39 +0700

    pemindahan konten publik

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/layouts/UserLayout";
 import AdminLayout from "./components/admin/AdminLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
