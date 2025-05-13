3dc00ee23aececa6f61476e5985f9a4a2a9846fa
shlmita <shelomita.fitriyani@gmail.com>
2025-05-13 16:53:51 +0700

    pemindahan konten publik

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -3,8 +3,8 @@ import { motion, AnimatePresence } from "framer-motion";
 
 
 const slides = [
-    { image: "/public/proyek-1.jpeg", title: "Boyong Grobog", description: "Hari jadi Kabupaten Grobogan Ke-294" },
-    { image: "/public/proyek-2.jpeg", title: "Penghargaan dari Kantor Pelayanan Pajak Pratama Blora", description: "Deskripsi singkat slide kedua." },
+    { image: "/proyek-1.jpg", title: "Boyong Grobog", description: "Hari jadi Kabupaten Grobogan Ke-294" },
+    { image: "/proyek-2.jpg", title: "Penghargaan dari Kantor Pelayanan Pajak Pratama Blora", description: "Deskripsi singkat slide kedua." },
 ];
 
 const Carousel = () => {
